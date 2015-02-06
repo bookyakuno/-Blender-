f590942cc74e15460c6f7e4b8b41d30bb2fd5e7b
bookyakuno <tounyu_live@yahoo.co.jp>
2015-02-06 20:16:26 +0900

    Update Bookyakuno_Key_setting.py
    ▼▼このショートカット設定の推奨環境▼▼
    
    
    
    ■Mac(OS X)推奨
     (Cmdキーを使用しているので)
    
    ■Ctrlキーをホームポジション小指近くに設定
    (より多くのキーを押しやすくするため)
    
    ■5ボタンマウス推奨
    (エンターと削除を副ボタンに割り当てているので)
    
    
    ■LMBをセレクトマウスに設定
    (一般的なマウス操作に合わせるため)
    
    ■外部キーリマップアプリでMMBとRMB入れ替え
    (移動拡縮回転時の座標指定の操作を右クリックでやりたいので)
    
    
    ■「Wazou’s Pie Menu」アドオンを使用
    (3Dビューのヘッダーでできることの大半を修飾キー各種 + MMBに割り当て)
    Scripts-Blender/Wazou_Pie_Menus at master · pitiwazou/Scripts-Blender
    https://github.com/pitiwazou/Scripts-Blender/blob/master/Wazou_Pie_Menus
    
    
     ■テンキー、「テンキーを模倣」を使用しない
    視点変更は、123/Ctrl + 123に割り当てて場所を節約
    数値入力は、karabinerを使用してQキーを押している間キーボード左側が一時的にテンキーになるように設定

--- Bookyakuno_Key_setting.py
+++ Bookyakuno_Key_setting.py
@@ -10,6 +10,7 @@ def kmi_props_setattr(kmi_props, attr, value):
     except Exception as e:
         print("Warning: %r" % e)
 
+
 wm = bpy.context.window_manager
 kc = wm.keyconfigs.new(os.path.splitext(os.path.basename(__file__))[0])
 
