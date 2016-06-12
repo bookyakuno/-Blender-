6b2d5bdcc02f67349c02fe5a10838ebbc90abed2
bookyakuno <kiokuya@gmail.com>
2016-06-12 23:02:51 +0900

    Update rigify_select_pie_menu.py

--- rigify_select_pie_menu.py
+++ rigify_select_pie_menu.py
@@ -1,4 +1,11 @@
-# 3D Navigation_x TOOLBAR v1.2 - 3Dview Addon - Blender 2.5x
+###########################################
+
+# RigifyPicker Addon download Link
+#
+# Downloads | Salva Artero
+# http://salvadorartero.com/downloads/
+
+###########################################
 #
 # THIS SCRIPT IS LICENSED UNDER GPL,
 # please read the license block.
@@ -22,13 +29,15 @@
 # ##### END GPL LICENSE BLOCK #####
 
 bl_info = {
-    "name": "rigify select Pie menu",
-    "author": "Bookyakuno",
-    "version": (1, 0, 0),
-    "blender": (2,77,0),
-    "description": "rigify rig Quick Select.",
-    "location": "Pose mode > Ctrl + W",
-    "category": "Pie menu",}
+	"name": "rigify select Pie menu",
+	"author": "Bookyakuno",
+	"version": (1, 0, 0),
+	"blender": (2,77,0),
+	"description": "rigify rig Quick Select. ",
+	"location": "Pose mode > Ctrl + W",
+	"category": "Pie menu",
+    "warning": "Use RigifyPicker Addon !!  http://salvadorartero.com/downloads/" 
+    }
 
 
 import bpy
@@ -36,33 +45,69 @@ from bpy.types import Menu
 
 
 
+import bpy, os
+from bpy.types import Menu, Header
+from bpy.props import IntProperty, FloatProperty, BoolProperty
+import bmesh
+from mathutils import *
+import math
+
+
+
+class rigify_select_Prefs(bpy.types.AddonPreferences):
+    bl_idname = __name__
+    bpy.types.Scene.Enable_Tab_01 = bpy.props.BoolProperty(default=False)
+
+
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(context.scene, "Enable_Tab_01", text="Info", icon="QUESTION")
+        if context.scene.Enable_Tab_01:
+
+            row = layout.row()
+            layout.label(text="timeline")
+            layout.label(text="RigifyPicker Addon download Link")
+
+
+            row.operator("wm.url_open", text="Downloads | Salva Artero").url = "http://salvadorartero.com/downloads/"
+
+
+
+
+
+
+
+
+
+
+
 ##################
 # Pie Menu Class #
 ##################
 class rigify_select_x(Menu):
-    bl_idname = "object.rigify_select"
-    bl_label = "Add Modifier"
+	bl_idname = "object.rigify_select"
+	bl_label = "Add Modifier"
 
-    def draw(self, context):
-        layout = self.layout
-        pie = layout.menu_pie()
+	def draw(self, context):
+		layout = self.layout
+		pie = layout.menu_pie()
 
-        #4 - LEFT
-        pie.operator("operador.mano_r", text="hand.ik.R", icon='HAND')
-        #6 - RIGHT
-        pie.operator("operador.mano_l", text="hand.ik.L", icon='HAND')
-        #2 - BOTTOM
-        pie.operator("operador.torso", text="Torso", icon='MONKEY')
-        #8 - TOP
-        pie.operator("operador.cabeza", text="Head", icon='BLENDER')
-        #7 - TOP - LEFT
-        pie.operator("operador.rodilla_r", text="foot_ROOL.R", icon='CURSOR')
-        #9 - TOP - RIGHT
-        pie.operator("operador.rodilla_l", text="foot_ROOL.L", icon='CURSOR')
-        #1 - BOTTOM - LEFT
-        pie.operator("operador.pie_r", text="foot.R", icon='EXPORT')
-        #3 - BOTTOM - RIGHT
-        pie.operator("operador.pie_l", text="foot.L", icon='EXPORT')
+		#4 - LEFT
+		pie.operator("operador.mano_r", text="hand.ik.R", icon='HAND')
+		#6 - RIGHT
+		pie.operator("operador.mano_l", text="hand.ik.L", icon='HAND')
+		#2 - BOTTOM
+		pie.operator("operador.torso", text="Torso", icon='MONKEY')
+		#8 - TOP
+		pie.operator("operador.cabeza", text="Head", icon='BLENDER')
+		#7 - TOP - LEFT
+		pie.operator("operador.rodilla_r", text="foot_ROOL.R", icon='CURSOR')
+		#9 - TOP - RIGHT
+		pie.operator("operador.rodilla_l", text="foot_ROOL.L", icon='CURSOR')
+		#1 - BOTTOM - LEFT
+		pie.operator("operador.pie_r", text="foot.R", icon='EXPORT')
+		#3 - BOTTOM - RIGHT
+		pie.operator("operador.pie_l", text="foot.L", icon='EXPORT')
 
 
 
@@ -106,6 +151,6 @@ def unregister():
 
 
 if __name__ == "__main__":
-    register()
+	register()
 
-    #bpy.ops.wm.call_menu_pie(name="object.rigify_select")
+	#bpy.ops.wm.call_menu_pie(name="object.rigify_select")
