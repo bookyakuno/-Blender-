c2472d03867afc8b29d9a2d0e881dd9abc4591f7
bookyakuno <kiokuya@gmail.com>
2016-10-05 23:07:06 +0900

    separate_maskを追加

--- Sculpt_status_header.py
+++ Sculpt_status_header.py
@@ -1,18 +1,18 @@
 # ##### BEGIN GPL LICENSE BLOCK #####
 #
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
+#   This program is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License
+#   as published by the Free Software Foundation; either version 2
+#   of the License, or (at your option) any later version.
 #
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+#   GNU General Public License for more details.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software Foundation,
+#   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
 # ##### END GPL LICENSE BLOCK #####
 
@@ -23,8 +23,8 @@ bl_info = {
 	"name" : "Sculpt status header",
 	"author" : "bookyakuno",
 	"version" : (0, 2),
-	"blender" : (2, 77),
-	"location" : "Sculpt Mode > 3DView > header > Left",
+	"blender" : (2, 78),
+	"location" : "Sculpt Mode > 3DView > header > Left, separate_mask > shfit + alt + D",
 	"description" : "Sculpt smart status",
 	"warning" : "",
 	"wiki_url" : "",
@@ -87,25 +87,25 @@ def sculpt_header(self, context):
 		self.layout.prop(arm, "use_mirror_x",text="",icon="MOD_MIRROR")
 
 
-#	obj = context.active_object
-#	if obj:
-#			obj_type = obj.type
+#   obj = context.active_object
+#   if obj:
+#   		obj_type = obj.type
 
-#			if obj_type in {'ARMATURE'}:
+#   		if obj_type in {'ARMATURE'}:
 
-#				arm = context.active_object.data
-#				self.layout.prop(arm, "use_mirror_x",text="",icon="MOD_MIRROR")
+#   			arm = context.active_object.data
+#   			self.layout.prop(arm, "use_mirror_x",text="",icon="MOD_MIRROR")
 
 
-#	if mode_string == 'EDIT_ARMATURE':
-#    
-#		arm = context.active_object.data
-#		self.layout.prop(arm, "use_mirror_x")
+#   if mode_string == 'EDIT_ARMATURE':
+#
+#   	arm = context.active_object.data
+#   	self.layout.prop(arm, "use_mirror_x")
 
-#	if ob.type == 'ARMATURE' and ob.mode in {'EDIT', 'POSE'}:
+#   if ob.type == 'ARMATURE' and ob.mode in {'EDIT', 'POSE'}:
 
-#		arm = context.active_object.data
-#		self.layout.prop(arm, "use_mirror_x")
+#   	arm = context.active_object.data
+#   	self.layout.prop(arm, "use_mirror_x")
 
 
 	if context.sculpt_object:
@@ -114,6 +114,7 @@ def sculpt_header(self, context):
 		row = col.row(align=True)
 		# シンメトリー
 		sculpt = context.tool_settings.sculpt
+		row.scale_x = 0.5
 		row.prop(sculpt, "use_symmetry_x", text="X", toggle=True)
 		row.prop(sculpt, "use_symmetry_y", text="Y", toggle=True)
 		row.prop(sculpt, "use_symmetry_z", text="Z", toggle=True)
@@ -121,27 +122,68 @@ def sculpt_header(self, context):
 
 
 
-# 		toolsettings = context.tool_settings
-# 		settings = self.paint_settings(context)
-# 		brush = settings.brush
+#   	 toolsettings = context.tool_settings
+#   	 settings = self.paint_settings(context)
+#   	 brush = settings.brush
 #
 #
-# 		col.template_ID_preview(settings, "brush", new="brush.add", rows=3, cols=8)
+#   	 col.template_ID_preview(settings, "brush", new="brush.add", rows=3, cols=8)
 
 
 
 		# Dynatopo
-		row.separator()
-		if context.sculpt_object.use_dynamic_topology_sculpting:
-			row.operator("sculpt.dynamic_topology_toggle", icon='CANCEL', text="")
-		else:
-			row.operator("sculpt.dynamic_topology_toggle", icon='MOD_REMESH', text="")
+# row.separator()
+# if context.sculpt_object.use_dynamic_topology_sculpting:
+#	 row.operator("sculpt.dynamic_topology_toggle", icon='CANCEL', text="")
+# else:
+#	 row.operator("sculpt.dynamic_topology_toggle", icon='MOD_REMESH', text="")
+# row.prop(sculpt, "detail_size", text="")
+
+
+
+		#booleans_sculpt_v_0_0_2.py
+
+		WM = context.window_manager
+		toolsettings = context.tool_settings
+		sculpt = toolsettings.sculpt
+
+		if len(context.selected_objects) >= 1 :
+				#Detail Size
+				row.separator()
+				row = layout.row(align=True)
+				row.operator("object.update_dyntopo", text=" ", icon='FILE_TICK')
+				row.scale_x = 0.5
+				row.prop(WM, "detail_size", text="")
+#   			row.scale_x = 1.2
+				row.separator()
+				if context.sculpt_object.use_dynamic_topology_sculpting:
+					row.operator("sculpt.dynamic_topology_toggle", icon='CANCEL', text="")
+				else:
+					row.operator("sculpt.dynamic_topology_toggle", icon='MOD_REMESH', text="")
+
+				row.separator()
+
+				if not bpy.context.object.mode == 'SCULPT':
+						layout.prop(WM, "subdivide_mesh", text="Subdivide")
+						if WM.subdivide_mesh:
+								layout.prop(WM, "use_sharps", text="Sharp Edges")
+				layout.prop(WM, "smooth_mesh", text="", icon='MOD_SMOOTH')
+				layout.prop(WM, "update_detail_flood_fill", text="", icon='MOD_DECIM')
+
+
+#	def prop_unified_strength(parent, context, brush, prop_name, icon='NONE', text="", slider=False):
+#		ups = context.tool_settings.unified_paint_settings
+#		ptr = ups if ups.use_unified_strength else brush
+#		parent.prop(ptr, prop_name, icon=icon, text=text, slider=slider)
+
+
+
 
 
 
 ################################################################
 
-#		row.prop(brush, "stroke_method", text="")
+#   	row.prop(brush, "stroke_method", text="")
 
 		row.prop(sculpt, "detail_size", text="")
 
@@ -161,21 +203,81 @@ def texture_import(self, context):
 
 
 
-	# アドオンを有効にしたときの処理
+
+
+
+# class sculptmode_off_persp(bpy.types.Operator):
+# 	bl_idname = "object.sculptmode_off_persp"
+# 	bl_label = "sculptmode_off_persp"
+#
+#
+# 	def execute(self, context):
+# 		# v = bpy.context.user_preferences.view
+# 		bpy.ops.sculpt.sculptmode_toggle()
+# 		bpy.context.user_preferences.view.use_auto_perspective = False
+#
+# 		if bpy.context.object.mode == 'SCULPT':
+#
+# 			bpy.ops.sculpt.sculptmode_toggle()
+# 			bpy.context.user_preferences.view.use_auto_perspective = True
+#
+# 		return {'FINISHED'}
+
+
+
+class separate_mask(bpy.types.Operator):
+	bl_idname = "object.separate_mask"
+	bl_label = "separate_mask"
+
+
+	def execute(self, context):
+		bpy.ops.paint.hide_show(action='HIDE', area='MASKED') # マスク部分を非表示
+		bpy.ops.sculpt.sculptmode_toggle() # オブジェクトモードに戻す
+		bpy.ops.object.select_all(action='DESELECT') #全選択解除で最後に選択するものを複製したものだけにする
+		bpy.ops.object.editmode_toggle() # 編集モード
+		bpy.ops.mesh.select_all(action='DESELECT') #全選択解除
+		bpy.ops.mesh.reveal() # 隠しているものを表示
+		bpy.ops.mesh.duplicate_move() # 選択部分を複製
+		bpy.ops.mesh.edge_face_add() # 閉じたオブジェクトにする(F2)
+		bpy.ops.mesh.quads_convert_to_tris(quad_method='BEAUTY', ngon_method='BEAUTY') # 閉じた面を三角形化
+		bpy.ops.mesh.separate(type='SELECTED') # 選択部分を分離
+		bpy.ops.object.editmode_toggle() # オブジェクトモード
+		bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY') #重心に原点を配置して、回転しやすいように
+
+		return {'FINISHED'}
+
+
+
+
+addon_keymaps = []
 def register():
-	# オペレーターなどを登録
-# 	bpy.utils.register_module(__name__)
-	# ヘッダーメニューに項目追加
+	bpy.utils.register_module(__name__)
+
 	bpy.types.VIEW3D_HT_header.prepend(sculpt_header)
 	bpy.types.VIEW3D_PT_tools_brush_texture.append(texture_import)
 
-# アドオンを無効にしたときの処理
+	# bpy.utils.register_class(separate_mask)
+
+
+
+	wm = bpy.context.window_manager
+	km = wm.keyconfigs.addon.keymaps.new(name='Sculpt', space_type='EMPTY')
+	kmi = km.keymap_items.new(separate_mask.bl_idname, 'D', 'PRESS',  alt=True, shift=True)
+	addon_keymaps.append((km, kmi))
+
+
+
+
+
+
+
+
+
 def unregister():
-	# オペレーターなどを解除
-# 	bpy.utils.unregister_module(__name__)
-	# ヘッダーメニューの項目解除
 	bpy.types.VIEW3D_HT_header.remove(sculpt_header)
 	bpy.types.VIEW3D_PT_tools_brush_texture.remove(texture_import)
+
+	# bpy.utils.unregister_class(separate_mask)
 
 
 
