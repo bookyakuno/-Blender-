ee07637932e7cc6edd7b394977649c30567e64c0
bookyakuno <kiokuya@gmail.com>
2019-01-19 21:07:27 +0900

    Mask Tools ver 0,36,0 メニューを分類。実行ボタンを大きくした マスクを太らせる・小さくする機能を追加 スムース・太らせる・小さくする機能のリピート回数を設定できるように マスクをシャープにする機能を追加

--- maskTools_2-8/__init__.py
+++ maskTools_2-8/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Mask Tools",
     "author": "Stanislav Blinov,Yigit Savtur,Bookyakuno (2.8Update)",
-    "version": (0, 35,1),
+    "version": (0, 36,0),
     "blender": (2, 80,0),
     "location": "3d View > Properties shelf (N) > Sculpt",
     "description": "Tools for Converting Sculpt Masks to Vertex groups",
@@ -76,56 +76,114 @@ class MaskToolsPanel(Panel):
     def draw(self, context):
         layout = self.layout
 
-        vgroupHeader = layout.row(align = True)
-        vgroupHeader.label(text = "Vertex Group :", icon = 'GROUP_VERTEX')
+        ###############################################################
+        row = layout.row(align = True)
+        row.label(text = "Vertex Group :", icon = 'GROUP_VERTEX')
+        row = layout.row(align = True)
+        row.label(text = "Save Mask to VGroup")
 
-        vGroupButtons = layout.row()
-        vGroupButtons.operator("mesh.masktovgroup", text = "Create VGroup", icon = 'NONE')
-        vGroupButtons.operator("mesh.masktovgroup_append", text = "Add", icon = 'DISCLOSURE_TRI_RIGHT')
-        vGroupButtons.operator("mesh.masktovgroup_remove", text = "Remove", icon = 'DISCLOSURE_TRI_DOWN')
+        row = layout.row()
+        row.scale_y = 1.3
+        row.operator("mesh.masktovgroup", text = "Create VGroup", icon = 'GROUP_VERTEX')
+        row = layout.row(align = True)
+        row.operator("mesh.masktovgroup_append", text = "Add VGroup", icon = 'EXPORT')
+        row.operator("mesh.masktovgroup_remove", text = "Difference VGroup", icon = 'UNLINKED')
+
+        space = layout.row()
+
+        ###############################################################
+        row = layout.row(align = True)
+        row.label(text = "Mask :", icon = 'MOD_MASK')
+        row = layout.row(align = True)
+        row.label(text = "Import VGroup to Mask ")
+
+
+
+        row = layout.row(align = True)
+        row.scale_y = 1.3
+        row.operator("mesh.vgrouptomask_append", text = "Add", icon = 'IMPORT')
+        row.operator("mesh.vgrouptomask_remove", text = "Difference", icon = 'UNLINKED')
+        row = layout.row()
+        row.operator("mesh.vgrouptomask", text = "New Mask", icon='NONE')
 
         space = layout.row()
 
-        maskHeader = layout.row(align = True)
-        maskHeader.label(text = "Mask :", icon = 'MOD_MASK')
+        row = layout.row(align = True)
+        row.label(text = "Mask Smooth/Sharp :", icon = 'MOD_SMOOTH')
 
-        maskButtons = layout.row()
-        maskButtons.operator("mesh.vgrouptomask", text = "Create Mask", icon='NONE')
-        maskButtons.operator("mesh.vgrouptomask_append", text = "Add", icon = 'DISCLOSURE_TRI_RIGHT')
-        maskButtons.operator("mesh.vgrouptomask_remove", text = "Remove", icon = 'DISCLOSURE_TRI_DOWN')
+        row = layout.row(align = True)
+        # row.label(text = "Mask Smooth", icon = 'MOD_MASK')
+        row.scale_y = 1.3
+        row.operator("mesh.mask_smooth_all", text = "Smooth", icon = 'MOD_SMOOTH')
+        row.operator("mesh.mask_sharp", text = "Sharp", icon = 'IMAGE_ALPHA')
+
+        row = layout.row(align = False)
+        row.prop(bpy.context.scene,"mask_smooth_strength", text = "Mask Smooth Strength", icon='MOD_MASK',slider = True)
+
+
+        space = layout.row()
+
+
+
+        ###############################################################
+        row = layout.row(align = True)
+        row.label(text = "Mask Fat/Less :", icon = 'ONIONSKIN_ON')
+        row = layout.row(align = True)
+        row.scale_y = 1.3
+        row.operator("mesh.mask_fat", text = "Mask Fat", icon = 'KEY_HLT')
+        row.operator("mesh.mask_less", text = "Mask Less", icon = 'KEY_DEHLT')
+
+        row = layout.row(align = True)
+        row.prop(bpy.context.scene,"mask_fat_repeat", text = "Mask Fat Repeat", icon='MOD_MASK',slider = True)
+        row.prop(bpy.context.scene,"mask_less_repeat", text = "Mask Less Repeat", icon='MOD_MASK',slider = True)
+
+        space = layout.row()
+
+        ###############################################################
+        row = layout.row(align = True)
+        row.label(text = "Mask Edge/Cavity :", icon = 'EDGESEL')
+
+
+        row = layout.row(align = True)
+        # row.label(text = "Mask by Edges :", icon = 'MOD_MASK')
+        row.scale_y = 1.3
+        row.operator("mesh.mask_from_edges", text = "Mask by Edges", icon = 'EDGESEL')
+
+        row = layout.row(align = True)
+        row.prop(bpy.context.scene,"mask_edge_angle", text = "Edge Angle",icon='MOD_MASK',slider = True)
+        row.prop(bpy.context.scene,"mask_edge_strength", text = "Mask Strength", icon='MOD_MASK',slider = True)
 
         space = layout.row()
         space = layout.row()
 
-        maskEdgesHeader = layout.row(align = True)
-        maskEdgesHeader.label(text = "Mask by Edges :", icon = 'MOD_MASK')
-        maskEdgesHeader.operator("mesh.mask_from_edges", text = "Create", icon = 'NONE')
+        row = layout.row(align = True)
+        # row.label(text = "Mask by Cavity:", icon = 'MOD_MASK')
+        row.scale_y = 1.3
+        row.operator("mesh.mask_from_cavity", text = "Mask by Cavity", icon = 'STYLUS_PRESSURE')
 
-        maskEdges = layout.row(align = True)
-        maskEdges.prop(bpy.context.scene,"mask_edge_angle", text = "Edge Angle",icon='MOD_MASK',slider = True)
-        maskEdges.prop(bpy.context.scene,"mask_edge_strength", text = "Mask Strength", icon='MOD_MASK',slider = True)
-
-        space = layout.row()
-        space = layout.row()
-
-        maskCavityHeader = layout.row(align = True)
-        maskCavityHeader.label(text = "Mask by Cavity:", icon = 'MOD_MASK')
-        maskCavityHeader.operator("mesh.mask_from_cavity", text = "Create", icon = 'NONE')
-
-        maskCavity = layout.row(align = True)
-        maskCavity.prop(bpy.context.scene,"mask_cavity_angle", text = "Cavity Angle",icon='MOD_MASK',slider = True)
-        maskCavity.prop(bpy.context.scene,"mask_cavity_strength", text = "Mask Strength", icon='MOD_MASK',slider = True)
+        row = layout.row(align = True)
+        row.prop(bpy.context.scene,"mask_cavity_angle", text = "Cavity Angle",icon='MOD_MASK',slider = True)
+        row.prop(bpy.context.scene,"mask_cavity_strength", text = "Mask Strength", icon='MOD_MASK',slider = True)
 
         space = layout.row()
         space = layout.row()
 
-        maskCavityHeader = layout.row(align = True)
-        maskCavityHeader.label(text = "Mask Smooth", icon = 'MOD_MASK')
-        maskCavityHeader.operator("mesh.mask_smooth_all", text = "Smooth", icon = 'NONE')
 
-        maskCavity = layout.row(align = False)
-        maskCavity.prop(bpy.context.scene,"mask_smooth_strength", text = "Mask Smooth Strength", icon='MOD_MASK',slider = True)
 
+        ###############################################################
+        # row = layout.row(align = True)
+        # row.label(text = "Mask Misc :", icon = 'FORCE_VORTEX')
+        #
+        # space = layout.row()
+        # row = layout.row(align = True)
+        # row.operator("mesh.mask_sharp_thick", text = "Mask Sharp (Thick)", icon = 'NONE')
+        # row = layout.row(align = True)
+        # maskCavity = layout.row(align = False)
+        # maskCavity.prop(bpy.context.scene,"mask_sharp_thick", text = "Mask Sharp Thick Strength", icon='MOD_MASK',slider = True)
+        #
+        # space = layout.row()
+        # row = layout.row(align = True)
+        # row.operator("mesh.duplicate", text = "Mask Duplicate")
 
 
 
@@ -149,9 +207,11 @@ VertexGroupToMaskRemove,
 MaskFromCavity,
 MaskFromEdges,
 MaskSmoothAll,
-
-
-
+MaskFat,
+MaskLess,
+MaskSharp,
+MaskSharpThick,
+MaskDuplicate,
 }
 
 
--- maskTools_2-8/maskFromCavity.py
+++ maskTools_2-8/maskFromCavity.py
@@ -193,7 +193,7 @@ class MaskSmoothAll(bpy.types.Operator):
         return context.active_object is not None and context.active_object.mode == 'SCULPT'
 
 
-    bpy.types.Scene.mask_smooth_strength = bpy.props.FloatProperty(name = "Mask Smooth Strength", default = 0.25, min = 0.1, max = 1.0)
+    bpy.types.Scene.mask_smooth_strength = bpy.props.IntProperty(name = "Mask Smooth Strength", default = 1, min = 0)
 
 
     def execute(self, context):
@@ -204,43 +204,263 @@ class MaskSmoothAll(bpy.types.Operator):
 
         if context.active_object.mode == 'SCULPT' :
 
-           if context.sculpt_object.use_dynamic_topology_sculpting :
-
-              dynatopoEnabled = True
-
-              bpy.ops.sculpt.dynamic_topology_toggle()
-
-           bmeshContainer = bmesh.new() # New bmesh container
-
-           bmeshContainer.from_mesh(context.active_object.data) # Fill container with our object
-
-           mask = bmeshContainer.verts.layers.paint_mask.active # get active mask layer
-
-           bmeshContainer.verts.ensure_lookup_table() # Update vertex lookup table
-
-           for vert in bmeshContainer.verts :
-
-                for edge in vert.link_edges :
-
-                    if vert [mask] < (edge.other_vert(vert) [mask] * abs(vert [mask]- mask_smooth_strength)):
-
-                       vert [mask]  = (edge.other_vert(vert) [mask] * abs(vert [mask] -mask_smooth_strength))
-
-                    if vert [mask]< 0.0 :
-
-                        vert [mask] = 0.0
-
-                    elif vert [mask]> 1.0 :
-
-                        vert [mask] = 1.0
-
-
-           bmeshContainer.to_mesh(context.active_object.data) # Fill obj data with bmesh data
-
-           bmeshContainer.free() # Release bmesh
+           bpy.ops.mesh.masktovgroup()
+           bpy.ops.mesh.masktovgroup_append()
+           bpy.ops.sculpt.sculptmode_toggle()
+           bpy.ops.paint.weight_paint_toggle()
+           bpy.ops.object.vertex_group_smooth(factor=1,repeat=mask_smooth_strength)
+           bpy.ops.paint.weight_paint_toggle()
+           bpy.ops.sculpt.sculptmode_toggle()
+           bpy.ops.paint.mask_flood_fill(mode='VALUE', value=0)
+           bpy.ops.mesh.vgrouptomask_append()
+           bpy.ops.object.vertex_group_remove(all=False, all_unlocked=False)
+           # if context.sculpt_object.use_dynamic_topology_sculpting :
+           #
+           #    dynatopoEnabled = True
+           #
+           #    bpy.ops.sculpt.dynamic_topology_toggle()
+           #
+           # bmeshContainer = bmesh.new() # New bmesh container
+           #
+           # bmeshContainer.from_mesh(context.active_object.data) # Fill container with our object
+           #
+           # mask = bmeshContainer.verts.layers.paint_mask.active # get active mask layer
+           #
+           # bmeshContainer.verts.ensure_lookup_table() # Update vertex lookup table
+           #
+           # for vert in bmeshContainer.verts :
+           #
+           #      for edge in vert.link_edges :
+           #
+           #          if vert [mask] < (edge.other_vert(vert) [mask] * abs(vert [mask]- mask_smooth_strength)):
+           #
+           #             vert [mask]  = (edge.other_vert(vert) [mask] * abs(vert [mask] -mask_smooth_strength))
+           #
+           #          if vert [mask]< 0.0 :
+           #
+           #              vert [mask] = 0.0
+           #
+           #          elif vert [mask]> 1.0 :
+           #
+           #              vert [mask] = 1.0
+           #
+           #
+           # bmeshContainer.to_mesh(context.active_object.data) # Fill obj data with bmesh data
+           #
+           # bmeshContainer.free() # Release bmesh
 
            if dynatopoEnabled :
+               bpy.ops.sculpt.dynamic_topology_toggle()
 
+        return {'FINISHED'}
+
+class MaskFat(bpy.types.Operator):
+    ''' Mask Fat '''
+    bl_idname = "mesh.mask_fat"
+    bl_label = "Mask Fat"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+
+    def poll(cls, context):
+
+        return context.active_object is not None and context.active_object.mode == 'SCULPT'
+
+    bpy.types.Scene.mask_fat_repeat = bpy.props.IntProperty(name = "Mask Fat Repeat", default = 1)
+
+    def execute(self, context):
+        mask_fat_repeat = context.scene.mask_fat_repeat # update property from user input
+
+
+        dynatopoEnabled = False
+
+        if context.active_object.mode == 'SCULPT' :
+
+           bpy.ops.mesh.masktovgroup()
+           bpy.ops.mesh.masktovgroup_append()
+           bpy.ops.sculpt.sculptmode_toggle()
+           bpy.ops.paint.weight_paint_toggle()
+           for var in range(0, mask_fat_repeat):
+               bpy.ops.object.vertex_group_smooth(factor=1,repeat=1,expand=1.0)
+               bpy.ops.object.vertex_group_levels(gain=1)
+           bpy.ops.paint.weight_paint_toggle()
+           bpy.ops.sculpt.sculptmode_toggle()
+           bpy.ops.paint.mask_flood_fill(mode='VALUE', value=0)
+           bpy.ops.mesh.vgrouptomask_append()
+           bpy.ops.object.vertex_group_remove(all=False, all_unlocked=False)
+
+
+           if dynatopoEnabled :
+               bpy.ops.sculpt.dynamic_topology_toggle()
+
+        return {'FINISHED'}
+
+class MaskLess(bpy.types.Operator):
+    ''' Mask Less '''
+    bl_idname = "mesh.mask_less"
+    bl_label = "Mask Less"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+
+    def poll(cls, context):
+
+        return context.active_object is not None and context.active_object.mode == 'SCULPT'
+    bpy.types.Scene.mask_less_repeat = bpy.props.IntProperty(name = "Mask Less Repeat", default = 1)
+
+
+    def execute(self, context):
+        mask_less_repeat = context.scene.mask_less_repeat # update property from user input
+
+
+        dynatopoEnabled = False
+
+        if context.active_object.mode == 'SCULPT' :
+           bpy.ops.paint.mask_flood_fill(mode='INVERT')
+           bpy.ops.mesh.masktovgroup()
+           bpy.ops.mesh.masktovgroup_append()
+           bpy.ops.sculpt.sculptmode_toggle()
+           bpy.ops.paint.weight_paint_toggle()
+           for var in range(0, mask_less_repeat):
+               bpy.ops.object.vertex_group_smooth(factor=1,repeat=1,expand=1.0)
+               bpy.ops.object.vertex_group_levels(gain=1)
+           bpy.ops.paint.weight_paint_toggle()
+           bpy.ops.sculpt.sculptmode_toggle()
+           bpy.ops.paint.mask_flood_fill(mode='VALUE', value=0)
+           bpy.ops.mesh.vgrouptomask_append()
+           bpy.ops.object.vertex_group_remove(all=False, all_unlocked=False)
+           bpy.ops.paint.mask_flood_fill(mode='INVERT')
+
+
+           if dynatopoEnabled :
+               bpy.ops.sculpt.dynamic_topology_toggle()
+
+        return {'FINISHED'}
+
+class MaskSharp(bpy.types.Operator):
+    ''' Mask Sharp '''
+    bl_idname = "mesh.mask_sharp"
+    bl_label = "Mask Sharp"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+
+    def poll(cls, context):
+
+        return context.active_object is not None and context.active_object.mode == 'SCULPT'
+
+
+    def execute(self, context):
+
+
+        dynatopoEnabled = False
+
+        if context.active_object.mode == 'SCULPT' :
+
+           bpy.ops.mesh.masktovgroup()
+           bpy.ops.mesh.masktovgroup_append()
+           bpy.ops.sculpt.sculptmode_toggle()
+           bpy.ops.paint.weight_paint_toggle()
+           bpy.ops.object.vertex_group_invert()
+           bpy.ops.object.vertex_group_smooth(factor=1,repeat=3,expand=1.0)
+           bpy.ops.object.vertex_group_levels(gain=5)
+           bpy.ops.object.vertex_group_quantize(steps=1)
+           bpy.ops.object.vertex_group_invert()
+           # bpy.ops.object.vertex_group_invert()
+
+
+           bpy.ops.paint.weight_paint_toggle()
+           bpy.ops.sculpt.sculptmode_toggle()
+           bpy.ops.paint.mask_flood_fill(mode='VALUE', value=0)
+           bpy.ops.mesh.vgrouptomask_append()
+           bpy.ops.object.vertex_group_remove(all=False, all_unlocked=False)
+
+
+           if dynatopoEnabled :
+               bpy.ops.sculpt.dynamic_topology_toggle()
+
+        return {'FINISHED'}
+
+class MaskSharpThick(bpy.types.Operator):
+    ''' Mask Sharp Thick '''
+    bl_idname = "mesh.mask_sharp_thick"
+    bl_label = "Mask Sharp (Thick)"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+
+    def poll(cls, context):
+
+        return context.active_object is not None and context.active_object.mode == 'SCULPT'
+
+
+    bpy.types.Scene.mask_sharp_thick = bpy.props.IntProperty(name = "Mask Sharp Thick", default = 50, min = 0)
+
+
+
+
+    def execute(self, context):
+        mask_sharp_thick = context.scene.mask_sharp_thick # update property from user input
+
+
+        dynatopoEnabled = False
+
+        if context.active_object.mode == 'SCULPT' :
+
+           bpy.ops.mesh.masktovgroup()
+           bpy.ops.mesh.masktovgroup_append()
+           bpy.ops.sculpt.sculptmode_toggle()
+           bpy.ops.paint.weight_paint_toggle()
+           bpy.ops.object.vertex_group_levels(gain=mask_sharp_thick)
+           bpy.ops.paint.weight_paint_toggle()
+           bpy.ops.sculpt.sculptmode_toggle()
+           bpy.ops.paint.mask_flood_fill(mode='VALUE', value=0)
+           bpy.ops.mesh.vgrouptomask_append()
+           bpy.ops.object.vertex_group_remove(all=False, all_unlocked=False)
+
+
+           if dynatopoEnabled :
+               bpy.ops.sculpt.dynamic_topology_toggle()
+
+        return {'FINISHED'}
+
+class MaskDuplicate(bpy.types.Operator):
+    ''' Mask Duplicate '''
+    bl_idname = "mesh.duplicate"
+    bl_label = "Mask Duplicate"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+
+    def poll(cls, context):
+
+        return context.active_object is not None and context.active_object.mode == 'SCULPT'
+
+    def execute(self, context):
+        mask_sharp_thick = context.scene.mask_sharp_thick # update property from user input
+
+
+        dynatopoEnabled = False
+
+        if context.active_object.mode == 'SCULPT' :
+           bpy.ops.paint.hide_show(action='HIDE', area='MASKED') # マスク部分を非表示
+           bpy.ops.sculpt.sculptmode_toggle() # オブジェクトモードに戻す
+           bpy.ops.object.select_all(action='DESELECT') #全選択解除で最後に選択するものを複製したものだけにする
+           bpy.ops.object.editmode_toggle() # 編集モード
+           bpy.ops.mesh.select_all(action='DESELECT') #全選択解除
+           bpy.ops.mesh.reveal() # 隠しているものを表示
+           #   	 bpy.ops.mesh.duplicate_move(MESH_OT_duplicate={"mode":1}, TRANSFORM_OT_translate={"value":(0, 0, 0))
+           bpy.context.scene.tool_settings.use_mesh_automerge = False
+           bpy.ops.mesh.duplicate_move(MESH_OT_duplicate=None, TRANSFORM_OT_translate=None)
+           # bpy.ops.mesh.duplicate_move() # 選択部分を複製
+           #   	bpy.ops.mesh.edge_face_add() # 閉じたオブジェクトにする
+           #   	bpy.ops.mesh.quads_convert_to_tris(quad_method='BEAUTY', ngon_method='BEAUTY') # 閉じた面を三角形化
+           bpy.ops.mesh.separate(type='SELECTED') # 選択部分を分離
+           bpy.ops.object.editmode_toggle() # オブジェクトモード
+           bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY') #重心に原点を配置して、回転しやすいように
+           bpy.context.scene.tool_settings.use_mesh_automerge = True
+
+           if dynatopoEnabled :
                bpy.ops.sculpt.dynamic_topology_toggle()
 
         return {'FINISHED'}
@@ -257,6 +477,7 @@ def register():
     bpy.types.Scene.mask_cavity_strength = MaskFromCavity.mask_cavity_strength
 
     bpy.types.Scene.mask_smooth_strength = MaskSmoothAll.mask_smooth_strength
+    bpy.types.Scene.mask_sharp_thick = MaskSmoothAll.mask_sharp_thick
 
     bpy.utils.register_module(__name__)
 
@@ -274,6 +495,7 @@ def unregister():
     bpy.types.Scene.mask_cavity_strength
 
     bpy.types.Scene.mask_smooth_strength
+    bpy.types.Scene.mask_sharp_thick
 
     bpy.utils.unregister_module(__name__)
 
