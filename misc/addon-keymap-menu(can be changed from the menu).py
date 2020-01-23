fd98ccfb3ee4f5034f60978777577d36d6de58fc
bookyakuno <kiokuya@gmail.com>
2020-01-24 00:02:52 +0900

    プロパティが違う場合でもエラーが起こらないように変更。カテゴリー名は1度のみ表示するように変更
    オペレーター名が同じで、プロパティが違う場合でもエラーが起こらないように変更
    カテゴリー名は1度のみ表示するように変更

--- misc/addon-keymap-menu(can be changed from the menu).py
+++ misc/addon-keymap-menu(can be changed from the menu).py
@@ -6,6 +6,7 @@ col.label(text="Keymap List:",icon="KEYINGSET")
 
 wm = bpy.context.window_manager
 kc = wm.keyconfigs.user
+old_km_name = ""
 for km_add, kmi_add in addon_keymaps:
     for km_con in kc.keymaps:
         if km_add.name == km_con.name:
@@ -13,12 +14,21 @@ for km_add, kmi_add in addon_keymaps:
 
     for kmi_con in km.keymap_items:
         if kmi_add.name == kmi_con.name:
-            kmi = kmi_con
+            # ここから  オペレーター名が同じで、プロパティが違う場合は、プロパティが同じものを判定する(例は"axis_x")。プロパティを設定していなければここは不要
+            try:
+                ad_pro = kmi_add.properties
+                con_pro = kmi_con.properties
+                if ad_pro.axis_x == con_pro.axis_x:
+                    kmi = kmi_con
+            except:
+                # ここまで
+                kmi = kmi_con
     try:
-        col.label(text=str(km.name),icon="DOT")
+        if not km.name == old_km_name:
+            col.label(text=str(km.name),icon="DOT")
         col.context_pointer_set("keymap", km)
         rna_keymap_ui.draw_kmi([], kc, km, kmi, col, 0)
         col.separator()
+        old_km_name = km.name
     except: pass
-
 
