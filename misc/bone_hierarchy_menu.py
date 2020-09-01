baa2945d72379f2cf4b6da5ae20a7453dfdb0856
bookyakuno <kiokuya@gmail.com>
2020-09-02 01:22:57 +0900

    再帰関数を利用して、より正しい書き方に作り直した
    スクショ画像
    https://twitter.com/Bookyakuno/status/1300831358621609984

--- misc/bone_hierarchy_menu.py
+++ misc/bone_hierarchy_menu.py
@@ -1,38 +1,55 @@
-# ボーンを階層表示する
-col = layout.column(align=True)
-if not bpy.context.object == None:
-  obj = bpy.context.object
-  if obj.type == "ARMATURE":
-    for bone in obj.data.bones: # 選択オブジェクトの全ボーンの中の (編集モードではeditbones)
-      if not bone.parent: # 親がない(一番上)のボーンの
-        col.prop(bone ,"name",text="",icon="ADD")
+import bpy
 
-        for bone in bone.children: # 1つ子のボーン
-          sp = col.split(align=True,factor=0.15)
-          sp.label(text=str(len(bone.children_recursive)),icon="NONE") # 下階層の子ボーンの数
-          sp.prop(bone ,"name",text="",icon="NONE")
 
-          for bone in bone.children:
-            bone_loop(bone,col,8) # メニュー。下記のfor文を内側に繰り返すことができれば行けそう
-            for bone in bone.children:
-              bone_loop(bone,col,9)
-              for bone in bone.children:
-                bone_loop(bone,col,10)
-                for bone in bone.children:
-                  bone_loop(bone,col,11)
-                  for bone in bone.children:
-                    bone_loop(bone,col,12)
-                    for bone in bone.children:
-                      bone_loop(bone,col,13)
-                      for bone in bone.children:
-                        bone_loop(bone,col,14)
-          col.separator()
-        col.separator()
+# ボーンを階層表示するメニュー
+def bone_hierarchy_menu(self,context,layout):
+	if not bpy.context.active_object:
+		return
+	if not bpy.context.object.type == "ARMATURE":
+		return
 
-        
+	obj = bpy.context.object
+	bone_data = obj.data.bones
 
-def bone_loop(bone,col,range_c):
+	col = layout.column(align=True)
+
+	# ボーンの最上階層(親なし)
+	top_bone_l = [b for b in bone_data if not b.parent]
+
+	for bone in top_bone_l:
+		# アクティブなボーンのアイコン
+		try:
+			if bone.name == bone_data.active.name:
+				icon_val = "BONE_DATA"
+			else:
+				icon_val = "BLANK1"
+		except: icon_val = "BLANK1"
+
+		if not bone.parent and bone.children: # トップ
+			col.prop(bone ,"name",text="top",icon=icon_val)
+
+		if not bone.children: # 子がない(一番した)
+			col.prop(bone ,"name",text="no child",icon=icon_val)
+			continue
+
+		# メイン
+		bone_hierarchy_loop(obj,bone,col,icon_val,1)
+		col.separator()
+
+
+# ループ
+def bone_hierarchy_loop(obj,bone,col,icon_val,count):
+	for bone in bone.children:
+		bone_child_item_menu(obj,bone,col,icon_val,count)
+		bone_hierarchy_loop(obj,bone,col,icon_val,count + 1)
+
+	if not bone.children:
+		return
+
+
+# 各ボーンのメニュー
+def bone_child_item_menu(obj,bone,col,icon_val,range_count):
 	rows = col.row(align=True)
-	for i in range(range_c):
-		rows.separator()
-	rows.prop(bone ,"name",text="",icon="NONE")
+	for i in range(range_count):
+		rows.separator(factor=2)
+	rows.prop(bone ,"name",text="",icon=icon_val)
